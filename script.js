c141856f65043a68497cd15570f66f6bf98febc1
Yazmeen Vazquez <yazmeen.vazquez14@gmail.com>
2017-01-17 23:44:21 +0000

    art

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //Write your function calls below this line
+var color = "black";
+setColor(0, 0, color);
+setColor(9, 0, color);
+setColor(0, 9, color);
+setColor(9, 9, color);
+
+var color = "purple";
+setColor(1, 1, color);
+setColor(1, 2, color);
+setColor(1, 3, color);
+setColor(2, 1, color);
+setColor(3, 1, color);
+setColor(3, 2, color);
+setColor(3, 3, color);
+setColor(4, 3, color);
+setColor(5, 3, color);
+setColor(5, 2, color);
+setColor(5, 1, color);
+
+var color = "yellow";
+setColor(2, 5, color);
+setColor(2, 8, color);
+setColor(4, 5, color);
+setColor(5, 6, color);
+setColor(5, 7, color);
+setColor(4, 8, color);
+
+setColor(7, 3, "green");
+setColor(7, 5, "red");
+setColor(8, 3, "green");
+setColor(8, 5, "red");
